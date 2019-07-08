2ca0189198d4bd2d2182146b321af5ce57b3ab6e
AustrainDeveloper <40561661+WaiseFass@users.noreply.github.com>
2019-07-08 10:41:56 +0200

    adds ImageButton

---
+++ ImageButton/ImageButton/ImageButton/Controls/ImageButton.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ImageButton
+{
+    public class ImageButton : Image
+    {
+        public ImageButton()
+        {
+            Initialize();
+        }
+
+        public static BindableProperty CommandProperty =
+            BindableProperty.Create(nameof(Command), typeof(ICommand), typeof(ImageButton));
+
+        public ICommand Command
+        {
+            get => (ICommand) GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+
+        public static BindableProperty CommandParameterProperty =
+            BindableProperty.Create(nameof(CommandParameter), typeof(object), typeof(ImageButton));
+
+        public object CommandParameter
+        {
+            get => GetValue(CommandParameterProperty);
+            set => SetValue(CommandParameterProperty, value);
+        }
+
+        public event EventHandler ItemTapped = (e, a) => { };
+
+        public void Initialize()
+        {
+            GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command =  TapCommand
+            });
+        }
+
+        private ICommand TapCommand => new Command(async () =>
+        {
+            AnchorX = 0.48;
+            AnchorY = 0.48;
+            await this.ScaleTo(0.8, 50, Easing.Linear);
+            await Task.Delay(100);
+            await this.ScaleTo(1, 50, Easing.Linear);
+
+            ItemTapped(this, EventArgs.Empty);
+        });
+    }
+}
---
+++ ImageButton/ImageButton/ImageButton/Helpers/PlatformImageExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ImageButton
+{
+    public class PlatformImageExtensions : IMarkupExtension<string>
+    {
+        public string SourceImage { get; set; }
+
+        public string ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (SourceImage == null)
+                return null;
+
+            string imagePath;
+
+            switch (Device.RuntimePlatform)
+            {
+                case Device.Android:
+                    imagePath = SourceImage;
+                    break;
+
+                case Device.iOS:
+                    imagePath = SourceImage + ".png";
+                    break;
+
+                case Device.UWP:
+                    imagePath = "Assets/" + SourceImage + ".png";
+                    break;
+
+                default:
+                    throw new ArgumentException();
+            }
+
+            return imagePath;
+        }
+
+        object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+        {
+            return ProvideValue(serviceProvider);
+        }
+    }
+}
